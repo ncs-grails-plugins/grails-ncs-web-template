85aec7288e6d3f09548199c511276862ad574a17
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-03-03 03:33:40 +0000

    fixing config for web template
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs-grails-plugins/grails-ncs-web-template/trunk@256 87bbd5ad-132b-4bfb-b67c-16c9f46f48d1

--- NcsWebTemplateGrailsPlugin.groovy
+++ NcsWebTemplateGrailsPlugin.groovy
@@ -7,7 +7,8 @@ class NcsWebTemplateGrailsPlugin {
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/error.gsp",
+			"grails-app/conf/"
     ]
 
     // TODO Fill in these fields
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,32 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+// Plugin Publishing Repository Information
+grails.project.repos.umnNcs.url = "http://artifact.ncs.umn.edu/plugins-release-local"
+grails.project.repos.umnNcs.type = "maven"
+grails.project.repos.default = "umnNcs"
+grails.release.scm.enabled = false
+
+// Dependency Resolution
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+}
+codenarc.reports = {
+	JenkinsXmlReport('xml') {
+		outputFile = 'target/test-reports/CodeNarcReport.xml'
+		title = 'CodeNarc Report for NCS People Plugin'
+	}
+	JenkinsHtmlReport('html') {
+		outputFile = 'CodeNarcReport.html'
+		title = 'CodeNarc Report for NCS People Plugin'
+	}
+}
+codenarc.propertiesFile = 'grails-app/conf/codenarc.properties'
+
